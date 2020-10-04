bc5647c8e0105e6535468aaca94ddb8320fadb2f
Varisht <varishtsg@gmail.com>
2020-10-04 14:57:36 +0530

    Use pyautogui instead of pynput

--- vol.py
+++ vol.py
@@ -1,20 +1,23 @@
 from pynput.keyboard import Key,Controller
 import time
+import pyautogui
 
-keyboard = Controller()
+# keyboard = Controller()
 def soundChange(percentChange):
 	if abs(percentChange)>0.2:
 		print(abs(int(percentChange*10)))
-		for i in range(abs(int(percentChange*5))):
+		for _ in range(abs(int(percentChange*5))):
 			if percentChange > 0 :
 				print("Increasing vol.........")
-				keyboard.press(Key.media_volume_up)
-				keyboard.release(Key.media_volume_up)
+				# keyboard.press(Key.media_volume_up)
+				# keyboard.release(Key.media_volume_up)
+				pyautogui.press('volumeup')
 				time.sleep(0.01)
 			else:
 				print("Decreasing vol.........")
-				keyboard.press(Key.media_volume_down)
-				keyboard.release(Key.media_volume_down)
+				# keyboard.press(Key.media_volume_down)
+				# keyboard.release(Key.media_volume_down)
+				pyautogui.press('volumedown')
 				time.sleep(0.01)
 	time.sleep(0.2) 
 
